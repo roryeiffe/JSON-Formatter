9d4202572f2b4ab251627ea478fe17448a8e20f2
RoryEiffe <roryeiffe8@gmail.com>
2025-06-03 13:31:58 -0400

    Added feature that assists in automating the activity mapping of a unit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,10 +5,13 @@ export const Header: React.FC = () => {
     <header className="bg-gradient-to-r from-indigo-600 to-blue-500 text-white py-8 px-4 shadow-lg rounded-b-lg">
       <div className="max-w-7xl mx-auto flex items-center justify-center">
         <Link className="text-3xl font-bold text-left mx-4 cursor-pointer hover:bg-blue-700 p-2" to = '/activities'>
-          Activity Creation
+          Activity Editor
         </Link>  
         <Link className="text-3xl font-bold text-left mx-4 cursor-pointer hover:bg-blue-700 p-2" to = '/excel'>
           Excel Uploader  
+        </Link>
+        <Link className="text-3xl font-bold text-left mx-4 cursor-pointer hover:bg-blue-700 p-2" to = '/mapping'>
+          Activity Mapping Util 
         </Link>
 
 
