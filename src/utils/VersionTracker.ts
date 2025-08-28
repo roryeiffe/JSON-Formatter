f8cdc0bc5a2327b7c840f2e03cbe4fb15332e20b
RoryEiffe <roryeiffe8@gmail.com>
2025-08-28 13:00:00 -0400

    Fixed version-tracker

--- src/utils/VersionTracker.ts
+++ src/utils/VersionTracker.ts
@@ -1,4 +1,4 @@
-const version = 'v1.6';
+const version = 'v1.7';
 
 export const returnVersionComment = () => {
     return `// Version: ${version} - This comment is auto-generated. Do not edit manually.`;
